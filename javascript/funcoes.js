e2be089cfeff1cb23f78885aa0f8512dc9e19d56
Renan Rodolfo da Silva <renaner123@gmail.com>
2020-11-13 21:27:23 -0200

    Funcoes do sudoky

---
+++ javascript/funcoes.js
@@ -0,0 +1,25 @@
+function clearBoard(){
+    alert("Clear");
+    stop();
+};
+
+function newBoard(){
+    alert("New");
+};
+
+function solve(){
+    alert("Solve");
+};
+
+function checkRow(){
+
+}
+
+function checkBoard(){
+
+    const array_board = [9][9];
+
+    console.log("oi")
+
+}
+
