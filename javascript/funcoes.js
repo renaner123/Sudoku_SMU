067e9e459540b2661c43793202401b5717c98297
Renan Rodolfo da Silva <renaner123@gmail.com>
2020-11-13 22:21:33 -0200

    Update

--- javascript/funcoes.js
+++ javascript/funcoes.js
@@ -1,3 +1,5 @@
+<script src="https://unpkg.com/peerjs@1.3.1/dist/peerjs.min.js"></script>
+
 function clearBoard(){
     alert("Clear");
     stop();
@@ -17,9 +19,16 @@ function checkRow(){
 
 function checkBoard(){
 
-    const array_board = [9][9];
-
-    console.log("oi")
-
 }
+
+function webRtc(){
+    var peer = new Peer(); 
+    var conn = peer.connect('another-peers-id');
+    // on open will be launch when you successfully connect to PeerServer
+    conn.on('open', function(){
+    // here you have conn.id
+    conn.send('hi!');
+    });
+}
+
 
