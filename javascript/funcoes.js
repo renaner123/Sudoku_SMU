ff866baa8c339aa926fed9b82978f347cdcd7e57
Renan Rodolfo da Silva <renaner123@gmail.com>
2020-11-16 12:02:59 -0300

    Funcao para gerar matrix na interface

--- javascript/funcoes.js
+++ javascript/funcoes.js
@@ -1,20 +1,75 @@
-function clearBoard(){
+var matrix = [];
+matrix =[[8,3,5,4,1,6,9,2,7], //vou receber uma matrix do back para manipular na interface
+                    [2,9,6,8,5,7,4,3,1],
+                    [4,1,7,2,9,3,6,5,8],
+                    [5,6,9,1,3,4,7,8,2],
+                    [1,2,3,6,7,8,5,4,9],
+                    [7,4,8,5,2,9,1,6,3],
+                    [6,5,2,7,8,1,3,9,4],
+                    [9,8,1,3,4,5,2,7,6],
+                    [3,7,4,9,6,2,8,1,5]];
+
+
+function clearBoard(){ //vai voltar pa matrix que o newBoard gerou
     alert("Clear");
 };
 
-function newBoard(){
-    alert("New");
+function newBoard(){ // joga matrix para interface, no caso, uma matrix para ser resolvida,
+
+    var dificuldade = document.getElementById("cDifficulty");
+
+    // vai enviar um requisição com a dificuldade e vai receber uma matrix correspondente.
+    // células vazias devem vir como " " na matriz.
+    var cell = "";
+    var pos = 0;
+    for(var i =0;i<matrix.length;i++){
+        for(var y=0; y< matrix[0].length;y++){
+            cell = document.getElementById("cell-"+pos);
+            cell.value = matrix[i][y];    
+            pos++;
+        }
+    }
 };
 
-function solve(){
-    alert("Solve");
+function solve(){ //envia a "matrix" que o usuário preencheu pra verificar com a matrix completa.
+
+var matrixCheck = [];
+matrixCheck =[[,,,,,,,,], 
+                    [,,,,,,,,],
+                    [,,,,,,,,],
+                    [,,,,,,,,],
+                    [,,,,,,,,],
+                    [,,,,,,,,],
+                    [,,,,,,,,],
+                    [,,,,,,,,],
+                    [,,,,,,,,]];
+
+var cell = "";
+var pos = 0;
+for(var i =0;i<matrix.length;i++){ //recebe valores do front e insere na matriz para comparar com o gabarito
+        for(var y=0; y< matrix.length;y++){
+            cell = document.getElementById("cell-"+pos);
+            matrixCheck[i][y]= cell.value;
+            pos++;
+        }
+ }
+ checkBoard(matrixCheck);
+ 
 };
 
-function checkRow(){
-
-}
-
-function checkBoard(){
+function checkBoard(args){ //Verificar se o que o usuário enviar no solve está correto
+   
+    var validate = true;
+    for(var i =0;i<matrix.length;i++){ //recebe valores do front e insere na matriz para comparar com o gabarito
+            for(var y=0; y< matrix.length;y++){
+                if(matrix[i][y]==args[i][y]){
+                    continue;
+                }else{
+                     validate = false;
+                }
+            }
+     }
+     (validate==false) ? alert("Try Gain") : alert("Gz");
 
 }
 
@@ -27,5 +82,4 @@ function webRtc(){
     conn.send('hi!');
     });
 }
-
 
